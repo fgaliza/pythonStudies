ef8922db15b349bbea749cc885f470d3c98064fe
Fillipe Galiza <fillipegaliza@hotmail.com>
2017-05-25 17:43:54 -0300

    String.py and new method to Calculator.py

--- Formatter.py
+++ Formatter.py
@@ -4,8 +4,13 @@ class Formatter(object):
 		super(Formatter, self).__init__()
 		self.arg = arg
 
-	def removeAccents():
+	def addStrings(string1, string2):
+		return string1 + string2
+	
+	def multiplyString(string1, n1):
+		return string1 * n1
 
+	def removeAccents():
 
 	def removeSpecialCharacters():
 
